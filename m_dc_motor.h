a826735885c408937e886b576c952519a652ea14
rustam.guseynov <rustam.guseynov@thomsonreuters.com>
2016-10-03 10:22:11 +0300

    Implementation -> source file

--- m_dc_motor.h
+++ m_dc_motor.h
@@ -14,36 +14,13 @@ class MDcMotor {
 private:
 	byte M1_PIN, M2_PIN, PWM_PIN;
 public:
-	MDcMotor(byte M1_PIN, byte M2_PIN, byte PWM_PIN) {
-		this->M1_PIN = M1_PIN;
-		this->M2_PIN = M2_PIN;
-		this->PWM_PIN = PWM_PIN;
-	}
+	MDcMotor(byte M1_PIN, byte M2_PIN, byte PWM_PIN);
 	
-	void init() {
-		pinMode(M1_PIN, OUTPUT);
-		pinMode(M2_PIN, OUTPUT);
-		pinMode(PWM_PIN, OUTPUT);
-	}
-	
-	void forward() {
-		digitalWrite(M1_PIN, HIGH);
-		digitalWrite(M2_PIN, LOW);
-	}
-
-	void reardrive() {
-		digitalWrite(M1_PIN, LOW);
-		digitalWrite(M2_PIN, HIGH);
-	}
-
-	void halt() {
-		digitalWrite(M1_PIN, LOW);
-		digitalWrite(M2_PIN, LOW);
-	}
-	
-	void setSpeed(byte speed) {
-		analogWrite(PWM_PIN, speed);
-	}
+	void init();
+	void forward();
+	void reardrive();
+	void halt();
+	void setSpeed(byte speed);
 };
 
 #endif
